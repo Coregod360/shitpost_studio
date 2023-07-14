e83a7885f4c5fdcfded706e166e918c83cfe51ce
Coregod360 <kevin_de_haan@hotmail.com>
2023-07-14 14:18:20 +0200

    add file tiles, add file detail pane

--- core/main.js
+++ core/main.js
@@ -1,15 +1,28 @@
-const { app, BrowserWindow } = require('electron')
-const serve = require('electron-serve')
-const serveDist = serve({directory: 'dist'})
+const { app, BrowserWindow } = require('electron');
+const serve = require('electron-serve');
+const serveDist = serve({directory: 'dist'});
+const ipc = require('electron').ipcMain;
 
+const path = require('path');
+const fs = require('fs');
+
+ipc.on('get-images', (event, arg) => {
+	// Doesn't work and needs to be dynamicly determined from the user settings anyway
+	let dir = path.join('home', 'kensix', 'Pictures', 'Shitposting', 'anime');
+	let files = fs.readdirSync("/home/kensix/Pictures/Shitposting/anime/");
+	event.reply('images', {files : files, dir : '/home/kensix/Pictures/Shitposting/anime/'});
+})
 
 app.whenReady().then(
 	async () => {
 		const window = new BrowserWindow({
-			width: 530,
-			height: 290,
+			width: 800,
+			height: 600,
 			autoHideMenuBar: true,
-			
+			webPreferences: {
+				nodeIntegration: true,
+				contextIsolation: false,
+			}
 		})
 
 		await serveDist(window)
--- ui/App.svelte
+++ ui/App.svelte
@@ -1,7 +1,120 @@
+<script>
+	import Layout from './Layout.svelte';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import { AppRail, AppRailTile } from '@skeletonlabs/skeleton';
+    import { onMount } from 'svelte';
+
+
+	const { ipcRenderer: ipc } = require('electron');
+
+
+	function getImages() {
+		ipc.send('get-images');
+	}
+
+	function selectFile(file){
+		selectedFile = file;
+	}
+
+	function closeFileDetails(){
+		selectedFile = '';
+	}
+
+	ipc.on('images', (event, data) => {
+			console.log(data.files);
+			console.log(data.dir);
+			images = data.files;
+			dir = data.dir;
+	});
+
+	onMount(() => {
+		getImages();
+	});
+
+	let dir = '';
+	let images = [];
+	let videos = [];
+	let currentTile = 0;
+
+	let selectedFile = '';
+	
+</script>
+
 <main>
-	<h1 class="text-2xl font-bold underline">
-		Hello world!
-	</h1>
+	<Layout>
+		<AppShell class='h-screen'>
+			<!-- SIDEBAR LEFT -->
+			<svelte:fragment slot="sidebarLeft">
+				<AppRail>
+					<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+						<svelte:fragment slot="lead"><i class="fa-solid fa-image text-2xl" /></svelte:fragment>
+						<span>Images</span>
+					</AppRailTile>
+					<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+						<svelte:fragment slot="lead"><i class="fa-solid fa-film text-2xl" /></svelte:fragment>
+						<span>Videos</span>
+					</AppRailTile>
+					<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
+						<svelte:fragment slot="lead"><i class="fa-solid fa-gear text-2xl"></i> </svelte:fragment>
+						<span>Settings</span>
+					</AppRailTile>
+				</AppRail>
+			</svelte:fragment>
+
+
+			<!-- HEADER -->
+			<svelte:fragment slot="pageHeader">
+				<div class="text-center py-4">
+					<!-- TODO: Add autocomplete from skeletonUI -->
+					<input class="input rounded-lg w-56 px-2" type="text" placeholder="Search" />
+				</div>
+			</svelte:fragment>
+
+
+			<!-- SIDEBAR RIGHT -->
+			<svelte:fragment slot="sidebarRight">
+				<!-- Display if file is selected showing file details and options -->
+				{#if selectedFile !== ''}
+					<button class="btn btn-secondary" on:click={closeFileDetails}>close</button>
+					<h1 class="text-center">{selectedFile}</h1>
+					<p class="text-center"> Image details etc lalalallalalalllalalal</p>
+				{/if}
+			</svelte:fragment>
+			
+
+			<!-- CONTENT  -->
+			<div class="rounded-none h-screen grid grid-cols-[auto_1fr] w-full p-5">
+				<!-- <div class="grid place-content-center place-items-center"> -->
+					{#if currentTile === 0}
+						
+						<!-- <span class="badge variant-soft">Filebrowser all</span> -->
+						
+						<div class="w-full text-token grid grid-cols-3 gap-4">
+							{#if images !== []}
+								{#each images as file}
+								<button class="card hover: bg-surface-100 rounded-md overflow-hidden" on:click={selectFile(file)}>
+									<header>
+										<img src="" class="bg-black/50 w-full aspect-[1/1]" alt="file" />
+									</header>
+									<div class="p-4 space-y-4">
+										<p>{file}</p>
+									</div>
+								</button>
+								{/each}
+							{/if}
+						</div>
+
+					{:else if currentTile === 1}
+						<span class="badge variant-soft">Filebrowser images</span>
+
+					{:else if currentTile === 2}
+						<span class="badge variant-soft">Filebrowser videos</span>
+					{/if}
+				</div>
+			<!-- </div> -->
+		</AppShell>
+
+	</Layout>
 </main>
 
 <style global>
