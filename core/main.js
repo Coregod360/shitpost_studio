2957a9eabdb2e0380bd440e5a4518ee1a74e1ee7
Coregod360 <kevin_de_haan@hotmail.com>
2023-08-05 07:02:01 +0200

    add ui, add IPC, add python support, add config

--- core/main.js
+++ core/main.js
@@ -2,32 +2,94 @@ const { app, BrowserWindow } = require('electron');
 const serve = require('electron-serve');
 const serveDist = serve({directory: 'dist'});
 const ipc = require('electron').ipcMain;
-
+const { spawn } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
-ipc.on('get-images', (event, arg) => {
-	// Doesn't work and needs to be dynamicly determined from the user settings anyway
-	let dir = path.join('home', 'kensix', 'Pictures', 'Shitposting', 'anime');
-	let files = fs.readdirSync("/home/kensix/Pictures/Shitposting/anime/");
-	event.reply('images', {files : files, dir : '/home/kensix/Pictures/Shitposting/anime/'});
+
+const userDataPath = app.getPath('userData');
+let config;
+let imagePath;
+let videoPath;
+
+// TODO: Maybe needs to be done in a better way
+async function getUserConfig(){
+	config = fs.readFileSync(path.join(userDataPath, '/config.json'));
+	let parsedConfig = JSON.parse(config);
+	imagePath = parsedConfig.imageBasePath;
+	videoPath = parsedConfig.videoBasePath;
+}
+
+
+ipc.on('get-user-config', (event, arg) => {
+	let config = fs.readFileSync(path.join(userDataPath, '/config.json'));
+	let parsedConfig = JSON.parse(config);
+	imagePath = parsedConfig.imageBasePath;
+	videoPath = parsedConfig.videoBasePath;
+	console.log(parsedConfig);
+	event.reply('user-config', parsedConfig);
 })
 
-app.whenReady().then(
-	async () => {
-		const window = new BrowserWindow({
-			width: 800,
-			height: 600,
-			autoHideMenuBar: true,
-			webPreferences: {
-				nodeIntegration: true,
-				contextIsolation: false,
-			}
-		})
 
-		await serveDist(window)
+ipc.on('get-images', (event, arg) => {
+	// TODO: made Dir dynamic and based on user settings Also test sub folders
+	let files = fs.readdirSync(imagePath);
+	event.reply('images', {images : files});
+	console.log('Getting images')
+})
 
-		window.loadURL('app://-')
-		window.webContents.openDevTools()
-	}
-)
+
+ipc.on('get-videos', (event, arg) => {
+	let files = fs.readdirSync(videoPath);
+	event.reply('videos', {videos : files});
+	console.log('Getting videos')
+})
+
+
+ipc.on('download-video', (event, arg) => {
+	console.log('Downloading video')
+})
+
+
+ipc.on('scan-images', (event, arg) => {
+	// TODO: Add python code to scan folder and label images 
+	console.log('Scanning images')
+	const subprocess = spawn('python', [
+		"-u", path.join(__dirname, '..', 'scripts', 'test.py'),
+	   "--foo", "some value for foo",
+	]);
+
+	subprocess.stdout.on('data', (data) => {
+		console.log(`data:${data}`);
+	});
+	subprocess.stderr.on('data', (data) => {
+		console.log(`error:${data}`);
+	});
+	subprocess.stderr.on('close', () => {
+		console.log("Closed");
+	});
+
+})
+
+
+getUserConfig().then(() => {
+	app.whenReady().then(
+		async () => {
+			const window = new BrowserWindow({
+				width: 1200,
+				height: 800,
+				autoHideMenuBar: true,
+				webPreferences: {
+					webSecurity: false,
+					nodeIntegration: true,
+					contextIsolation: false,
+				}
+			})
+
+			await serveDist(window)
+
+			window.loadURL('app://-')
+			window.webContents.openDevTools()
+		}
+	)
+})
--- ui/App.svelte
+++ ui/App.svelte
@@ -7,37 +7,113 @@
 
 	const { ipcRenderer: ipc } = require('electron');
 
+	let imageBasePath = '';
+	let videoBasePath = '';
+	let defaultVolume = '';
+
+
+	let images = [];
+	let videos = [];
+	let currentTile = 0;
+	let selectedFile = '';
+
+	// IPC funtions 
 
 	function getImages() {
 		ipc.send('get-images');
 	}
 
+	function getVideos() {
+		ipc.send('get-videos');
+	}
+
+	function getUserConfig() {
+		ipc.send('get-user-config');
+	}
+
+	function scanImages() {
+		ipc.send('scan-images');
+	}
+
+	function downloadVideo() {
+		ipc.send('download-video');
+	}
+
+	ipc.on('images', (event, data) => {
+			console.log(data);
+			images = data.images;
+			// imageBasePath = data.dir;
+	});
+
+	ipc.on('videos', (event, data) => {
+			console.log(data);
+			videos = data.videos;
+			// videoBasePath = data.dir;
+	});
+
+	ipc.on('user-config', (event, data) => {
+			console.log(data);
+			imageBasePath = data.imageBasePath;
+			videoBasePath = data.videoBasePath;
+			defaultVolume = data.defaultVolume;
+	});
+
+	//Page functions 
+	onMount(() => {
+		getUserConfig();
+	});
+
+
 	function selectFile(file){
-		selectedFile = file;
+		if (currentTile === 0){
+			selectedFile = file;
+		}
+		if (currentTile === 1){
+			selectedFile = file;
+			let video = document.getElementById('videoPreview');
+			console.log(video);
+			video.src = `file://${videoBasePath}${selectedFile}`;
+			// TODO: Make user dependent audio volume
+			// Has bug where first time selecting video it will play at full volume because get element returns null
+			video.volume = defaultVolume;
+			video.load(selectedFile);
+		}
+		
 	}
 
 	function closeFileDetails(){
 		selectedFile = '';
 	}
 
-	ipc.on('images', (event, data) => {
-			console.log(data.files);
-			console.log(data.dir);
-			images = data.files;
-			dir = data.dir;
-	});
+	function showfilepicker(){
+		showDirectoryPicker();
+	}
 
-	onMount(() => {
-		getImages();
-	});
-
-	let dir = '';
-	let images = [];
-	let videos = [];
-	let currentTile = 0;
-
-	let selectedFile = '';
+	// Resets file selection when switching between image and videos 
+	$: if(currentTile){
+		selectedFile = '';
+		console.log('reset file selection');
+	}
 	
+	$: if(currentTile === 0){
+		if(images.length === 0){
+			getImages();
+		}
+		// console.log('Images already loaded');
+	}
+
+	$: if(currentTile === 1){
+		if(videos.length === 0){
+			getVideos();
+		}
+		// console.log('videos already loaded');
+	}
+
+	$: if(currentTile === 2){
+		// Get settings
+		console.log('settings');
+	}
+
 </script>
 
 <main>
@@ -46,7 +122,7 @@
 			<!-- SIDEBAR LEFT -->
 			<svelte:fragment slot="sidebarLeft">
 				<AppRail>
-					<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
+					<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1" on:click{test}>
 						<svelte:fragment slot="lead"><i class="fa-solid fa-image text-2xl" /></svelte:fragment>
 						<span>Images</span>
 					</AppRailTile>
@@ -64,37 +140,68 @@
 
 			<!-- HEADER -->
 			<svelte:fragment slot="pageHeader">
+				{#if currentTile === 0}
 				<div class="text-center py-4">
 					<!-- TODO: Add autocomplete from skeletonUI -->
 					<input class="input rounded-lg w-56 px-2" type="text" placeholder="Search" />
+					<button class="btn btn-primary" on:click={scanImages}>scan folder</button>
 				</div>
+				{/if}
+
+				{#if currentTile === 1}
+				<div class="text-center py-4">
+					<input class="input rounded-lg w-56 px-2" type="text" placeholder="Search" />
+					<button class="btn btn-primary">download video</button>
+				</div>
+				{/if}
+
+				{#if currentTile === 2}
+					<!-- Nothing to do here  -->
+				{/if}
+
 			</svelte:fragment>
 
 
 			<!-- SIDEBAR RIGHT -->
 			<svelte:fragment slot="sidebarRight">
 				<!-- Display if file is selected showing file details and options -->
+				
 				{#if selectedFile !== ''}
-					<button class="btn btn-secondary" on:click={closeFileDetails}>close</button>
-					<h1 class="text-center">{selectedFile}</h1>
-					<p class="text-center"> Image details etc lalalallalalalllalalal</p>
+					{#if currentTile === 0}
+						<div class="flex flex-col max-w-md px-2 justify-center">
+							<button class="btn btn-secondary justify-end" on:click={closeFileDetails}><i class="fa-solid fa-x"></i></button>
+							<img src={`file://${imageBasePath}${selectedFile}`} class="w-screen" alt="file" />
+							<h1 class="text-center pt-2">{selectedFile}</h1>
+							<p> File info </p>
+						</div>
+					{/if}
+
+					{#if currentTile === 1}
+						<div class="flex flex-col max-w-md px-2 justify-center"> 
+							<button class="btn btn-secondary justify-end" on:click={closeFileDetails}><i class="fa-solid fa-x"></i></button>
+							<video id='videoPreview' class="w-screen" controls autoplay loop> 
+								<source src={`file://${videoBasePath}${selectedFile}`} type="video/mp4" />
+								<track kind="captions" />
+							</video>
+							<h1 class="text-center pt-2">{selectedFile}</h1>
+							<p> File info</p>
+						</div>
+					{/if}
 				{/if}
 			</svelte:fragment>
 			
 
 			<!-- CONTENT  -->
-			<div class="rounded-none h-screen grid grid-cols-[auto_1fr] w-full p-5">
-				<!-- <div class="grid place-content-center place-items-center"> -->
+			<!-- <div class="rounded-none h-screen grid grid-cols-[auto_1fr] w-full p-5"> -->
 					{#if currentTile === 0}
-						
-						<!-- <span class="badge variant-soft">Filebrowser all</span> -->
-						
-						<div class="w-full text-token grid grid-cols-3 gap-4">
+					<div class="rounded-none h-screen grid grid-cols-[auto_1fr] w-full p-5">
+						<div class="w-full text-token grid grid-cols-4 gap-4">
 							{#if images !== []}
 								{#each images as file}
 								<button class="card hover: bg-surface-100 rounded-md overflow-hidden" on:click={selectFile(file)}>
 									<header>
-										<img src="" class="bg-black/50 w-full aspect-[1/1]" alt="file" />
+										<!-- TODO: Fix image squishing  -->
+										<img src={`file://${imageBasePath}${file}`} class="bg-black/50 w-full aspect-square" alt="file" />
 									</header>
 									<div class="p-4 space-y-4">
 										<p>{file}</p>
@@ -103,15 +210,39 @@
 								{/each}
 							{/if}
 						</div>
+					</div>
 
 					{:else if currentTile === 1}
-						<span class="badge variant-soft">Filebrowser images</span>
+					<div class="rounded-none h-screen grid grid-cols-[auto_1fr] w-full p-5">
+						<div class="w-full text-token grid grid-cols-4 gap-4">
+							{#if videos !== []}
+								{#each videos as file}
+								<!-- TODO: Fix video not changing when selecting  -->
+								<button class="card hover: bg-surface-100 rounded-md overflow-hidden" on:click={selectFile(file)}>
+									<header>
+										<video class="bg-black/50 w-full aspect-square">
+											<source src={`file://${videoBasePath}${file}`} type="video/mp4" />
+											<track kind="captions" />
+										</video>
+									</header>
+									<div class="p-4 space-y-4">
+										<p>{file}</p>
+									</div>
+								</button>
+								{/each}
+							{/if}
+						</div>
+					</div>
 
 					{:else if currentTile === 2}
-						<span class="badge variant-soft">Filebrowser videos</span>
+						<h1 class="text-center"> settings </h1>
+						<div class="mx-auto max-w-lg">
+							<p>Choose image folder</p>
+							<p>{imageBasePath}</p>
+							<!-- TODO: Add all required settings and a way to push settings to the user config -->
+							<button class="btn btn-primary" on:click={showfilepicker}>pick folder</button>
+						</div>
 					{/if}
-				</div>
-			<!-- </div> -->
 		</AppShell>
 
 	</Layout>
